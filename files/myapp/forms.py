9d748f241619928d7a3bca59e8838554bd353e3e
Jijitha V <jijithavijitus@gmail.com>
2026-01-24 17:48:52 +0530

    foreign key updates

--- files/myapp/forms.py
+++ files/myapp/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from myapp.models import Student
+from myapp.models import Student,Course
 
 class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'age', 'profile_pic']
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['name', 'course_name']
--- files/myapp/models.py
+++ files/myapp/models.py
@@ -4,3 +4,10 @@ class Student(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     profile_pic = models.ImageField(upload_to="profile/", null=True, blank=True)
+
+def __str__(self):
+    return self.name
+
+class Course(models.Model):
+    name = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course_name = models.CharField(max_length=50)
